3ca4b053c04f1697cfc61f9939d7f4e7453bd216
wyynbkls <wangyy520925@qq.com>
2025-05-20 02:18:54 +0800

    Update MusicPlugin.java

--- plugins/hd/MusicPlugin/MusicPlugin.java
+++ plugins/hd/MusicPlugin/MusicPlugin.java
@@ -1,35 +1,35 @@
 
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
-import com.alibaba.fastjson2.JSONPath;
+进口com.alibaba.fastjson2.JSON；
+进口com.alibaba.fastjson2.JSONObject；
+进口com.alibaba.fastjson2.JSONPath；
 
-import me.hd.wauxv.plugin.api.callback.PluginCallBack;
+进口我。高清。wauxv.plugin.API.callback.PluginCallBack；
 
-void sendMusic(String talker, String title) {
-    get("https://api.vkeys.cn/v2/music/netease?word=" + title + "&choose=1", null, new PluginCallBack.HttpCallback() {
-        public void onSuccess(int respCode, String respContent) {
-            JSONObject jsonObject = JSON.parseObject(respContent);
-            String id = JSONPath.eval(jsonObject, "$.data.id").toString();
-            String name = JSONPath.eval(jsonObject, "$.data.song");
-            String singer = JSONPath.eval(jsonObject, "$.data.singer");
-            String url = JSONPath.eval(jsonObject, "$.data.url");
-            String link = JSONPath.eval(jsonObject, "$.data.link");
-            sendMusicCard(talker, name, singer, url, link, "wx8dd6ecd81906fd84");
+无效的 sendMusic(线 说话的人,线 标题) {
+    得到("https://api.vkeys.cn/v2/music/netease?word="+标题+"选择=1(&S)",无效的,新的 PluginCallBack.HttpCallback() {
+        公共 无效的 onSuccess(int respCode,线 respContent) {
+            JSONObject JSONObject=JSON。parseObject(respContent);
+            线 身份标识=JSONPath。eval(JSONObject，"$.data.id").toString();
+            线 姓名=JSONPath。eval(JSONObject，"$.data.song");
+            线 歌手=JSONPath。eval(JSONObject，"$.data.singer");
+            线 URL=JSONPath。eval(JSONObject，"$.data.url");
+            线 链接=JSONPath。eval(JSONObject，"$.data.link");
+            sendMusicCard(说话人，名字，歌手，url，链接，"wx8dd6ecd81906fd84");
         }
 
-        public void onError(Exception e) {
-            sendText(talker, "[落月API]请求异常:" + e.getMessage());
+        公共 无效的 onError(例外 e) {
+            sendText(说话的人，"[落月API]请求异常："+e.getMessage());
         }
     });
 }
 
-void onHandleMsg(Object msgInfoBean) {
-    if (msgInfoBean.isText()) {
-        String content = msgInfoBean.getContent();
-        String talker = msgInfoBean.getTalker();
-        if (content.startsWith("/点歌 ")) {
-            String title = content.substring(4);
-            sendMusic(talker, title);
+无效的 onHandleMsg(对象 msgInfoBean) {
+    如果 (msgInfoBean。IsText()) {
+        线 内容=msgInfoBean。getContent();
+        线 说话的人=msgInfoBean。getTalker();
+        如果 (内容。startswith("/点歌 ")) {
+            线 标题=内容。子字串(4);
+            sendMusic(谈话者，标题);
         }
     }
 }
